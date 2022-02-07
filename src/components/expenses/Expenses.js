07fa6936044e82d6c091ce610aca406d948aa26d
Colby Cocking <colbycocking@gmail.com>
2022-02-07 12:05:35 -0800

    I lied it was section 3 and here are some updates

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,26 +1,31 @@
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 export default function Expenses() {
   const expenseDate = new Date(2022, 2, 28);
   const expenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
+      title: 'Guns',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+    {
+      id: 'e2',
+      title: 'Gun Safe',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
     {
       id: 'e3',
-      title: 'Car Insurance',
+      title: 'Bullets',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
+      title: 'BFG',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
@@ -31,6 +36,7 @@ export default function Expenses() {
       <ExpenseItem date={expenseDate} itemProps={expenses[0]}/>
       <ExpenseItem date={expenseDate} itemProps={expenses[1]}/>
       <ExpenseItem date={expenseDate} itemProps={expenses[2]}/>
+      <ExpenseItem date={expenseDate} itemProps={expenses[3]}/>
     </Card>
   )
 }
